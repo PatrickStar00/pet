b7a70d6fe36d992fd23af684590137953be6ce63
adm <sergeykryt.ru@gmail.com>
2025-08-04 05:14:38 +1100

    add forms for log in

--- microservices/auth/login.py
+++ microservices/auth/login.py
@@ -1,21 +1,50 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
+from fastapi import APIRouter, Depends, Form, HTTPException, status
 import pwd_operations as operats
-from main import UserScheme
-from shemas import TokenInfo
+from sqlalchemy.ext.asyncio import AsyncSession
+from shemas import TokenInfo, UserScheme
+from models import AuthModel
+from sqlalchemy import select
+from database import get_session
 
 router = APIRouter(prefix="/jwt", tags=["JWT"])
 
-def validate_auth():
-    pass
+
+async def validate_auth(
+    login: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    session: AsyncSession = Depends(get_session)  # Зависимость для получения сессии
+):
+    result = await session.execute(
+        select(AuthModel).where(AuthModel.login == login)
+    )
+    user = result.scalar_one_or_none()
+
+    if not user or not operats.check_password(password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password"
+        )
+        
+    return user
+
+    # if not user:
+    #     raise HTTPException(status_code=404, detail="Пользователь не найден")
+
+    # if not operats.check_password(password, user.password):
+    #     raise HTTPException(status_code=401, detail="Неверный пароль")
+    # pass
 
 @router.post("/login/", response_model=TokenInfo)
 def auth_user(user: UserScheme = Depends(validate_auth)):
     
+    
+    
     jwt_payload = {
         "sub" : user.id,
-        "username" :user.username,
+        "username" :user.login,
     }
-    token = operats.encode_jwt({"sub": user.login})
+    token = operats.encode_jwt(payload=jwt_payload)
     return TokenInfo(access_token=token, token_type="Bearer")
 
     
