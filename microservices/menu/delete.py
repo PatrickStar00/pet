9b577668f3da10ab42074089b754be3dea80b5aa
adm <sergeykryt.ru@gmail.com>
2025-08-09 22:40:33 +1100

    add delete dish

---
+++ microservices/menu/delete.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Form, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+from database import get_session
+from models import MenuModel
+from sqlalchemy import select
+
+router = APIRouter(tags=["DELETE DISH"])
+
+@router.delete("/delete_dish")
+async def delete_dish(
+    id: Annotated[int, Form()],
+    name: Annotated[str, Form()],
+    session: AsyncSession = Depends(get_session)):
+    
+    if not id and not name:
+        raise HTTPException(status_code=400, detail="Укажите id или name для удаления блюда.")
+
+    # Поиск по id или по name
+    query = select(MenuModel).where(MenuModel.id == id) if id else select(MenuModel).where(MenuModel.name == name)
+    result = await session.execute(query)
+    dish = result.scalar_one_or_none()
+
+    if not dish:
+        raise HTTPException(status_code=404, detail="Блюдо не найдено.")
+
+    # Удаление
+    await session.delete(dish)
+    await session.commit()
+
+    return {"status": "success", "message": f"Блюдо '{dish.name}' удалено."}
