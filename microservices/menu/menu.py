54cbb5abbd8d0ffe734d16aca8b7c712f8658454
adm <sergeykryt.ru@gmail.com>
2025-09-08 00:11:31 +1100

    add endpoint to get price of dish

--- microservices/menu/menu.py
+++ microservices/menu/menu.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_session
 from models import MenuModel
 from sqlalchemy import select
+from typing import Annotated
 
 
 router = APIRouter(tags=["MENU"])
@@ -21,3 +22,17 @@ async def check_menu(session: AsyncSession = Depends(get_session)):
         }
         for item in menu_items
     ]
+    
+@router.get("/menu/{id}/price")
+async def get_price_by_id(
+    id: int,
+    session: AsyncSession = Depends(get_session)
+):
+    query = select(MenuModel.price).where(MenuModel.id == id)
+    result = await session.execute(query)
+    price = result.scalar_one_or_none()
+    
+    if price is None:
+        raise HTTPException(status_code=404, detail="Блюдо не найдено")
+    
+    return {"price": price}
